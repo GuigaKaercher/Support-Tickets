97046d3d5524b1eaffc3a32aa476124e74715049
Guilherme Kaercher <guigakaercher2@gmail.com>
2025-02-03 21:17:07 -0300

    rotas criadas!

---
+++ src/routes/tickets.js
@@ -0,0 +1,10 @@
+export const tickets = [
+    {
+        method: "POST",
+        path: "/tickets",
+        controller: (req , res) => {
+            Response.end(" criado com sucesso! ")
+        }  
+
+    }
+]
